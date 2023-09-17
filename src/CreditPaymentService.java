f022b44e10c970d493183b6f583ce1ce075e8671
leily <leily@example.com>
2023-09-17 16:32:26 +0300

    Change name and reformate code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,12 +1,9 @@
 public class CreditPaymentService {
 
-        public int calculate (int amount, double percent, int term) {
-            int result;
-            result = (int) (amount * (percent/12) * Math.pow( 1 + percent/12, term) / (Math.pow(1 + (percent/12),  term)-1));
-            return result;
-
-
-
+    public int calculate(int amount, double percent, int term) {
+        int result;
+        result = (int) (amount * (percent / 12) * Math.pow(1 + percent / 12, term) / (Math.pow(1 + (percent / 12), term) - 1));
+        return result;
 
 
     }
